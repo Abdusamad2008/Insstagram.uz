351fbb03685aeb20664ed4eee8b7d54ef82d35cb
Abdusamad2008 <cyberkolibri08@gmail.com>
2023-06-17 15:29:22 +0500

    17.06.2023

--- app/src/main/java/com/abdusamad/insstagram/ShareProfile.kt
+++ app/src/main/java/com/abdusamad/insstagram/ShareProfile.kt
@@ -1,14 +1,35 @@
 package com.abdusamad.insstagram
 
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.Context
 import android.os.Bundle
+import android.content.ClipboardManager
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.nio.file.CopyOption
+import java.nio.file.Files
 
 class ShareProfile:AppCompatActivity() {
+    val link ="https://t.me/Dasturchi_28"
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.share_profiel_layout)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        val link ="https://t.me/Dasturchi_28"
+        val copy_link_btn:Button=findViewById(R.id.copy_link_profile)
+        copy_link_btn.setOnClickListener {
+
+            val clipboardManager = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("text", "$link")
+            clipboardManager.setPrimaryClip(clipData)
+            Toast.makeText(this, "Text copied to clipboard", Toast.LENGTH_LONG).show()
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -17,4 +38,6 @@ class ShareProfile:AppCompatActivity() {
         }
         return true
     }
+
+
 }
